bce2e1e16aff9cce87aa18ee9199b3d07d5f48de
Yu-Jo Tseng <ytseng@hubspot.com>
2020-10-12 13:29:51 +0100

    Add navbar

--- project/start.js
+++ project/start.js
@@ -13,6 +13,18 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
